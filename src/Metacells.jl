012edc1c376dd268be94e77e85612641d4743f7e
Oren Ben-Kiki <oren@ben-kiki.org>
2024-03-27 16:34:12 +0200

    Fix links.

--- src/Metacells.jl
+++ src/Metacells.jl
@@ -1,10 +1,10 @@
 """
-The `Metacells.jl` package provides computational services for the `metacells <https://github.com/tanaylab/metacells>`_ package, using
-`Daf <https://github.com/tanaylab/Daf.jl>)`_ to hold the data. In the future, we'll ideally migrate all of the
-`metacells`package computations to this package, converting the Python package to a thin wrapper, and provide a similar
-thin R wrapper to provide metacell analysis from R as well. For now,`Metacells.jl`only provides a subset of the features
-of the Python`metacells`package, which requires users to convert data from`AnnData`(for the old features) to`Daf` (to
-the new features).
+The `Metacells.jl` package provides computational services for the [metacells](https://github.com/tanaylab/metacells)
+package, using [Daf](https://github.com/tanaylab/Daf.jl) to hold the data. In the future, we'll ideally migrate all of
+the `metacells`package computations to this package, converting the Python package to a thin wrapper, and provide a
+similar thin R wrapper to provide metacell analysis from R as well. For now,`Metacells.jl`only provides a subset of the
+features of the Python`metacells`package, which requires users to convert data from`AnnData`(for the old features)
+to`Daf` (to the new features).
 """
 module Metacells
 
